34cc28d2865bbdcf27cd8aefa5b78aba48fc7d6f
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-05-22 17:07:38 -0400

    Update main.php

--- src/main/main.php
+++ src/main/main.php
@@ -5,6 +5,8 @@ namespace main;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\Player;
 
 class Main extends PluginBase{
   
@@ -14,4 +16,13 @@ class Main extends PluginBase{
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
     {
-            if($cmd->getName()
+            if($cmd->getName() == "id") {
+              if($player->hasPermission("id.use"); {
+                $getid = $player->getInventory()->getItemInHand()->getId();
+                $player->sendMessage(" The ID of this item is $getid  ");
+              }
+                 }else{
+                   $sender->sendMessage("§4You Dont have permission to use this command");
+                 }
+                 }
+                 
