92e8775942fbc232a6010463ac89ba8443eb4099
secretsilver313 <64828266+secretsilver313@users.noreply.github.com>
2020-05-22 16:31:07 -0400

    Update main.php

--- src/main/main.php
+++ src/main/main.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace main;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+  
+  
+    public function onEnable() {
+  }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+            if($cmd->getName()
